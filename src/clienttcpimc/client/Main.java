841665efcc5a450377b34baeeeeb04a014943bd4
Gerson Muñoz <desarrolladorwebandnet5@gmail.com>
2024-08-17 11:46:02 -0500

    Adding files

---
+++ src/clienttcpimc/client/Main.java
@@ -0,0 +1,11 @@
+package clienttcpimc.client;
+
+import clienttcpimc.client.views.MainView;
+
+public class Main {
+    public static void main(String[] args) {
+        MainView v = new MainView();
+        v.setLocationRelativeTo(null);
+        v.setVisible(true);
+    }    
+}
